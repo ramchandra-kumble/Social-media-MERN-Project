7f4593f4f03ae26bbb7d729b351690b548a5669c
Vishal Nair <vishalnair111@gmail.com>
2021-06-09 00:05:20 +0530

    User Model updated

---
+++ Node-RestAPI/models/User.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        min:5,
+        max:20,
+        unique:true
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 50,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+        unique: true 
+    },
+    profilePicture: {
+        type: String,
+        default: ""
+    },
+    coverPicture: {
+        type: String,
+        default: ""
+    },
+    followers: {
+        type: Array,
+        default: []
+    },
+    followings: {
+        type: Array,
+        default: []
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+
+},
+ { timestamps: true }
+)
+// unique is used in this case to avoid username duplication
+// timestamps tells mongoose to automatically create and update properties to schema
+
+module.exports = mongoose.model("User", UserSchema);
