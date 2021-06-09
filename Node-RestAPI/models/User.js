d2d32cc73026f00c11f27e6f406d6973363c7003
Vishal Nair <vishalnair111@gmail.com>
2021-06-10 00:32:04 +0530

    User.js updated

--- Node-RestAPI/models/User.js
+++ Node-RestAPI/models/User.js
@@ -40,7 +40,23 @@ const UserSchema = new mongoose.Schema({
     isAdmin: {
         type: Boolean,
         default: false
-    }
+    },
+    desc:{
+        type: String,
+        max:50
+    },
+    city: {
+        type: String,
+        max:50
+    },
+    from:{
+        type:String,
+        max:50
+    },
+    relationship:{
+        type:Number,
+        enum: [1, 2, 3]
+    },
 
 },
  { timestamps: true }
