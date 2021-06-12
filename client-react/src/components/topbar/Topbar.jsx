3c5b189c6a4c315ea67b51e9ca8e056b1f91e4c9
vomi-exe <mehtavomi@gmail.com>
2021-06-12 16:57:39 +0530

    top bar issue resolved

--- client-react/src/components/topbar/Topbar.jsx
+++ client-react/src/components/topbar/Topbar.jsx
@@ -1,5 +1,5 @@
 import { Chat, Person, Search, Notifications } from "@material-ui/icons";
-import "./Topbar.css";
+import "./topbar.css";
 
 export default function Topbar() {
   return (
--- client-react/src/pages/home/home.jsx
+++ client-react/src/pages/home/home.jsx
@@ -3,7 +3,7 @@ import Topbar from "../../components/topbar/Topbar"
 import Sidebar from "../../components/sidebar/Sidebar"
 import Rightbar from "../../components/rightbar/Rightbar"
 import Feed from "../../components/feed/Feed"
-import "./home.css"
+import "./Home.css"
 
 
 
